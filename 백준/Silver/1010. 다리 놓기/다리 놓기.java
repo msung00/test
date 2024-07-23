9d9d21123a54557b630de2396747a6678355160f
msung00 <138764247+msung00@users.noreply.github.com>
2024-07-23 09:45:45 +0900

    [Silver V] Title: 다리 놓기, Time: 296 ms, Memory: 18688 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int t=0;t<T;t++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			int result = 1;
+			for(int i=1;i<=N;i++) {
+				result = result * (M-i+1) / i;
+			}
+			System.out.println(result);
+		}
+		
+	}
+}
