e46f6b6a1a81fafd9ab84ca6a9742174a8345949
msung00 <138764247+msung00@users.noreply.github.com>
2024-07-25 09:22:21 +0900

    [Bronze V] Title: 사파리월드, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextInt();
+        long b=sc.nextInt();
+        System.out.print(Math.abs(a-b));
+    }
+}
