63a6b6f56cf2f715bbf3a6f62e9caac1fa46f912
msung00 <138764247+msung00@users.noreply.github.com>
2024-08-21 09:45:51 +0900

    [Bronze II] Title: 나머지, Time: 164 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> h = new HashSet<Integer>();
+ 
+		for (int i = 0; i < 10; i++) {
+			h.add(sc.nextInt() % 42);
+        }
+        
+		System.out.print(h.size());
+	}
+}
